979cc134b8ca2a182cea6d1b7e5a0cefa3593a86
max <max.latuz@alumnos.uv.cl>
2024-11-08 20:33:52 -0300

    ajustes a rutass

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,9 +3,10 @@ import {signup, signin, logout} from "../controllers/auth.controllers.js";
 
 const router = Router();
 
-router.post("/auth/signup", signup);
+router.post("/auth/registrarse", signup);
 
-router.post("/auth/signin", signin);
-router.get("/auth/logout", logout);
+router.post("/auth/logearse", signin);
+
+router.get("/auth/deslogearse", logout);
 
 export default router;
