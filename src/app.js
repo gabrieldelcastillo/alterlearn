2098301587a5de2f50e93eaadf86678d7cb39938
max <max.latuz@alumnos.uv.cl>
2024-11-08 14:28:04 -0300

    base registro-login

--- src/app.js
+++ src/app.js
@@ -1,18 +1,63 @@
-import express from 'express'
-import employesRoutes from './routes/employes.routes.js'
-import indexRoutes from './routes/index.routes.js'
-import registerRoutes from './routes/register.routes.js'
-import loginRoutes from './routes/login.routes.js'
-import { errorHandler, errorRuta } from './middleware/errorHandler.middleware.js'
+import express from "express";
+import exphbs from "express-handlebars";
 
-const app = express()
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+import {PORT} from "./config.js";
 
-app.use(express.json())
-app.use(indexRoutes)
-app.use(employesRoutes)
-app.use(registerRoutes)
-app.use(loginRoutes)
-app.use(errorRuta)
-app.use(errorHandler)
+import middleware from './middleware/seguridad.middleware.js'
 
-export default app
+import indexRoutes from "./routes/index.routes.js";
+import notesRoutes from "./routes/notes.routes.js";
+import userRoutes from "./routes/auth.routes.js";
+import "./config/passport.js";
+
+// Initializations
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// settings
+app.set("port", PORT);
+app.set("views", join(__dirname, "views"));
+
+// config view engine
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  layoutsDir: join(app.get("views"), "layouts"),
+  partialsDir: join(app.get("views"), "partials"),
+  extname: ".hbs",
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+// middlewares
+middleware(app);
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+
+// routes
+app.use(indexRoutes);
+app.use(userRoutes);
+app.use(notesRoutes);
+
+// static files
+app.use(express.static(join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  return res.status(404).render("404");
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render("error", {
+    error,
+  });
+});
+
+export default app;
--- src/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express"
-import {loginUser} from "../controllers/login.controller.js"
-
-const router = Router();
-
-router.post('/alterlearn/login', loginUser);
-
-export default router;
